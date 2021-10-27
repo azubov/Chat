4c53e720d524f8b62d2d22d23273d7451fa7de87
artem <zubov@lanit.ru>
2021-10-27 19:06:19 +0300

    added view for input

--- src/main/java/ru/lanit/chat/client/controller/MessageController.java
+++ src/main/java/ru/lanit/chat/client/controller/MessageController.java
@@ -4,24 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import ru.lanit.chat.gen.Message;
 import ru.lanit.chat.messages.MessageRepository;
-import java.util.List;
 
 @Controller
+@RequestMapping("/chat")
 public class MessageController {
 
     @Autowired
     MessageRepository repository;
 
-    @GetMapping("/all")
+    @GetMapping
     public String allMessages(Model model) {
-        List<Message> messageList = repository.getMessages();
-        messageList.forEach(message -> System.out.println(message.getText()));
-        String text = messageList.get(0).getText();
-        model.addAttribute("message", text);
-        model.addAttribute("messageList", messageList);
+        model.addAttribute("messageList", repository.getMessages());
         return "messages";
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@ModelAttribute("message") Message message) {
+        repository.addMessage(message);
+        return "redirect:/chat";
     }
 
 
